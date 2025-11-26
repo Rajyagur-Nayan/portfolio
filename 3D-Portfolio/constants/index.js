954a4e2b6fb695343aa45075b31b9da56316fa9d
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-11-26 17:59:29 +0530

    first commit

---
+++ 3D-Portfolio/constants/index.js
@@ -0,0 +1,532 @@
+import GithubIcon from "./../public/assets/icons/github.svg";
+import LinkedInIcon from "./../public/assets/icons/linkedin.svg";
+import XIcon from "./../public/assets/icons/x.svg";
+import InstagramIcon from "./../public/assets/icons/instagram.svg";
+import FrontendIcon from "./../public/assets/icons/frontend.svg";
+import LeaderShipIcon from "./../public/assets/icons/leadership.svg";
+import ProblemSolvingIcon from "./../public/assets/icons/problem-solving.svg";
+import FreelancerIcon from "./../public/assets/icons/freelance.svg";
+import BackendIcon from "./../public/assets/icons/backend.svg";
+import FullStackIcon from "./../public/assets/icons/full-stack.svg";
+
+const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Software Developer",
+    icon: <FullStackIcon />,
+  },
+  {
+    title: "Frontend Developer",
+    icon: <FrontendIcon />,
+  },
+  // {
+  //   title: "Backend Developer",
+  //   icon: <BackendIcon />,
+  // },
+  {
+    title: "Problem Solving",
+    icon: <ProblemSolvingIcon />,
+  },
+  {
+    title: "Freelancer",
+    icon: <FreelancerIcon />,
+  },
+  // {
+  //   title: "Leadership",
+  //   icon: <LeaderShipIcon />,
+  // },
+];
+
+const technologies = {
+  languages: [
+    {
+      name: "HTML5",
+      icon: "/assets/tech/html5.svg",
+      link: "https://html.spec.whatwg.org/multipage/",
+    },
+    {
+      name: "CSS3",
+      icon: "/assets/tech/css3.svg",
+      link: "https://www.w3.org/Style/CSS/Overview.en.html",
+    },
+    {
+      name: "JavaScript",
+      icon: "/assets/tech/javascript.svg",
+      link: "https://262.ecma-international.org/",
+    },
+    {
+      name: "TypeScript",
+      icon: "/assets/tech/typescript.svg",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
+      name: "C",
+      icon: "/assets/tech/c.svg",
+      link: "https://en.cppreference.com/w/c",
+    },
+    {
+      name: "Java",
+      icon: "/assets/tech/java.svg",
+      link: "https://www.java.com/en/",
+    },
+    {
+      name: "Python",
+      icon: "/assets/tech/python.svg",
+      link: "https://www.python.org/",
+    },
+  ],
+  frameworks: [
+    {
+      name: "Next.js",
+      icon: "/assets/tech/nextjs.svg",
+      link: "https://nextjs.org/",
+    },
+    {
+      name: "TailwindCSS",
+      icon: "/assets/tech/tailwindcss.svg",
+      link: "https://tailwindcss.com/",
+    },
+    {
+      name: "Express.js",
+      icon: "/assets/tech/expressjs.png",
+      link: "https://expressjs.com/",
+    },
+    {
+      name: "Flutter",
+      icon: "/assets/tech/flutter.svg",
+      link: "https://flutter.dev/",
+    },
+  ],
+  libraries: [
+    {
+      name: "React",
+      icon: "/assets/tech/react.svg",
+      link: "https://react.dev/",
+    },
+    {
+      name: "Three.js",
+      icon: "/assets/tech/threejs.svg",
+      link: "https://threejs.org/",
+    },
+    {
+      name: "Styled-Components",
+      icon: "/assets/tech/styled-components.png",
+      link: "https://styled-components.com/",
+    },
+    {
+      name: "Framer-motion",
+      icon: "/assets/tech/framer.svg",
+      link: "https://www.framer.com/motion/",
+    },
+    {
+      name: "Zustand",
+      icon: "https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg",
+      link: "https://zustand-demo.pmnd.rs",
+    },
+    {
+      name: "Redux/Redux-toolkit",
+      icon: "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+      link: "https://redux.js.org",
+    },
+    {
+      name: "NextAuth.js",
+      icon: "/assets/tech/nextauthjs.png",
+      link: "https://next-auth.js.org/",
+    },
+    {
+      name: "Prisma",
+      icon: "/assets/tech/prisma.svg",
+      link: "https://www.prisma.io/",
+    },
+  ],
+  tools: [
+    {
+      name: "Git",
+      icon: "/assets/tech/git.svg",
+      link: "https://git-scm.com/",
+    },
+    {
+      name: "Github",
+      icon: "/assets/icons/github.svg",
+      link: "https://github.com/",
+    },
+    {
+      name: "Postman",
+      icon: "/assets/tech/postman.svg",
+      link: "https://www.postman.com/",
+    },
+    {
+      name: "Figma",
+      icon: "/assets/tech/figma.svg",
+      link: "https://www.figma.com/",
+    },
+    {
+      name: "Docker",
+      icon: "/assets/tech/docker.svg",
+      link: "https://www.docker.com/",
+    },
+  ],
+  environments: [
+    {
+      name: "Node.js",
+      icon: "/assets/tech/nodejs.svg",
+      link: "https://nodejs.org/en",
+    },
+  ],
+  databases: [
+    {
+      name: "MySQL",
+      icon: "/assets/tech/my-sql.png",
+      link: "https://www.mysql.com/",
+    },
+    {
+      name: "PostgreSQL",
+      icon: "/assets/tech/postgresql.png",
+      link: "https://www.postgresql.org",
+    },
+    {
+      name: "MongoDB",
+      icon: "/assets/tech/mongodb.svg",
+      link: "https://www.mongodb.com/",
+    },
+    {
+      name: "Firebase",
+      icon: "/assets/tech/firebase.svg",
+      link: "https://firebase.google.com/",
+    },
+  ],
+};
+
+const experiences = [
+  // {
+  //   title: "Full Stack Developer",
+  //   company_name: "Tech Lead at GDSC MVJCE",
+  //   icon: "/assets/company/gdsc-logo.svg",
+  //   iconBg: "#E6DEDD",
+  //   date: "July 2023 - Present",
+  //   points: [
+  //     "Currently serving as the Tech Lead of GDSC, responsible for guiding technical aspects, fostering collaboration, and contributing to strategic decisions.",
+  //     "Led induction programs, ensured new member alignment with GDSC's mission, and provided technical mentorship.",
+  //     "Developed GDSC MVJCE website, enhancing the organization's online visibility.",
+  //     "Spearheaded promotional campaigns, actively engaged with the GDSC community, and played a key role in planning and executing events.",
+  //   ],
+  // },
+  {
+    title: "Freelancing",
+    company_name: "Independent Freelancer",
+    icon: "/assets/company/curowell-logo.svg",
+    iconBg: "#E6DEDD",
+    date: "September 2025",
+    points: [
+      "Built and optimized healthcare SaaS solutions using Next.js, TypeScript, and Tailwind CSS, delivering smooth and intuitive user experiences.",
+      "Developed and maintained scalable applications independently while adhering to industry best practices.",
+      "Enhanced microfrontend modules such as DND forms, improving usability and workflow efficiency.",
+      "Contributed to UI/UX design and reusable component systems, reducing development time by nearly 25%.",
+      "📘 School Management System** – Designed a complete system for student data, attendance tracking, faculty handling, and admin dashboards.",
+      "🧑‍🏫 Faculty Muster & Attendance Website** – Built a streamlined attendance and muster tracking portal for teachers with real-time updates and reports.",
+      "📄 PDF Converter for CA Firms** – Created an automated PDF-to-document converter tailored for chartered accountants to simplify documentation workflows.",
+      "🏨 Hotel Management Website** – Developed a modern hotel booking and admin management platform with room listings, bookings, and customer management.",
+      "📚 Tuition / Coaching Institute Website** – Built an elegant website for tutoring services with course listings, student inquiry forms, and admin CMS.",
+    ],
+  },
+
+  // {
+  //   title: "Frontend Developer",
+  //   company_name: "Tech team member at GDSC MVJCE",
+  //   icon: "/assets/company/gdsc-logo.svg",
+  //   iconBg: "#E6DEDD",
+  //   date: "February 2022 - July 2023",
+  //   points: [
+  //     "Successfully planned and executed induction programs, providing new members with crucial information about the organization's goals and values.",
+  //     "Demonstrated web development expertise by conducting CSS workshops, equipping participants with practical web design and styling skills.",
+  //     "Played a crucial role in promoting GDSC events and actively collaborated with cross-functional teams to deliver high-quality projects, fostering teamwork and innovation.",
+  //     "Actively engaged with the GDSC community, assisted in workshops, and contributed to planning and managing GDSC events, ensuring their success and growth.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "AI-Powered Interview",
+    description:
+      "An AI-driven interview preparation website that simulates real interview scenarios using intelligent question generation, speech analysis, and performance scoring. The platform delivers instant feedback, highlights strengths and weaknesses, and helps users build confidence for technical and non-technical interviews.",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "next.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "cloudinary",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image:
+      "https://res.cloudinary.com/dqiqi75hm/image/upload/v1734187202/quickedit/dp6y8s8dstqfpq7svmj5.png",
+    source_code_link: "https://github.com/Rajyagur-Nayan/ai-interviewer-.git",
+    deployed_link: "https://ai-interviewer-sable.vercel.app",
+  },
+  {
+    name: "Student Complaint Management System",
+    description:
+      "A digital Student Complaint Portal designed to simplify how students report issues and receive support. The system provides a structured workflow for complaint submission, status tracking, and resolution, helping institutions improve communication, accountability, and student satisfaction",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "next.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "SQL",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Neon",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "/assets/projects/fig-pro.png",
+    source_code_link: "https://github.com/Rajyagur-Nayan/student-complaint.git",
+    deployed_link: "https://student-complaint-eosin.vercel.app",
+  },
+  // {
+  //   name: "GDSC Website",
+  //   description:
+  //     "Welcome to the official repository for the GDSC MVJCE Website, built with Next.js, Styled Components, and Prisma! This website serves as the central hub for all things tech-related at GDSC MVJCE. From insightful blog posts to incredible community projects, and from exciting tech events to getting to know our team, you'll find it all here.",
+  //   tags: [
+  //     {
+  //       name: "next.js",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "styled-components",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "three.js",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "prisma",
+  //       color: "orange-text-gradient",
+  //     },
+  //     {
+  //       name: "framer-motion",
+  //       color: "yellow-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/gdsc-website.png",
+  //   source_code_link: "https://github.com/GDSC-MVJCE/gdscmvjce-website.git",
+  //   deployed_link: "https://gdscmvjce.vercel.app/",
+  // },
+  {
+    name: "Ai-Health-Assistant",
+    description:
+      "AI-Health-Assistant is an AI-powered health advisory tool designed to help users understand their symptoms and get preliminary medical suggestions. The system leverages natural language processing and smart diagnosis logic to deliver fast, accurate, and accessible health insights.",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "node.js",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "tanstack/react-query",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "/assets/projects/facility-manager.png",
+    source_code_link: "https://github.com/Rajyagur-Nayan/ai-health-assistant-",
+  },
+  // {
+  //   name: "Netflix Clone",
+  //   description:
+  //     "Netflix-Clone is a fully responsive web application that replicates the popular streaming platform Netflix. It is built using Next.js, Prisma, React, NextAuth.js, Tailwind CSS, MongoDB and TypeScript. This project aims to showcase the implementation of these technologies to create a feature-rich clone of Netflix.",
+  //   tags: [
+  //     {
+  //       name: "next.js",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "typescript",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "tailwindcss",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "mongodb",
+  //       color: "orange-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/netflix-clone.png",
+  //   source_code_link: "https://github.com/Shivam-Sharma-1/Netflix-Clone.git",
+  //   deployed_link: "https://netflix-clone12345.vercel.app",
+  // },
+  // {
+  //   name: "Van-Life",
+  //   description:
+  //     "VanLife is a user-friendly website built with React that simplifies van searches. It is a web app used to browse different types of vans. It allows users to log in and offers convenient tag-based filtering for easy browsing.",
+  //   tags: [
+  //     {
+  //       name: "react",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "mirage.js",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "firebase-firestore",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: "/assets/projects/vanlife.png",
+  //   source_code_link: "https://github.com/Shivam-Sharma-1/Van-Life.git",
+  //   deployed_link: "https://myvanlife.netlify.app",
+  // },
+  // {
+  // 	name: "Spooky-Run",
+  // 	description:
+  // 		"Spooky Run is a web based 2D arcade style game that allows players to play as a lost dog finding its way back home through a spooky forest. The game is built using vanilla JavaScript and utilizes HTML5 and the 'canvas' element to render graphics and handle user input.",
+  // 	tags: [
+  // 		{
+  // 			name: "html5",
+  // 			color: "blue-text-gradient"
+  // 		},
+  // 		{
+  // 			name: "css3",
+  // 			color: "green-text-gradient"
+  // 		},
+  // 		{
+  // 			name: "javascript",
+  // 			color: "pink-text-gradient"
+  // 		}
+  // 	],
+  // 	image: "/assets/projects/spooky-run.png",
+  // 	source_code_link: "https://github.com/Shivam-Sharma-1/Spooky-Run.git",
+  // 	deployed_link: "https://shivam-sharma-1.github.io/Spooky-Run"
+  // }
+];
+
+const socials = [
+  {
+    id: "github",
+    icon: <GithubIcon />,
+    link: "https://github.com/Rajyagur-Nayan",
+  },
+  {
+    id: "linkedin",
+    icon: <LinkedInIcon />,
+    link: "https://www.linkedin.com/in/nayan-rajyaguru-18389629b/",
+  },
+  {
+    id: "instagram",
+    icon: <InstagramIcon />,
+    link: "https://www.instagram.com/rajyaguru_nayan?igsh=MThtMjVwMWQ5ZTZrdw==",
+  },
+];
+
+const heroTexts = [
+  "React/Next.js developer",
+  500,
+  "Freelancer",
+  500,
+  "Full-Stack developer",
+  500,
+  "Frontend developer",
+  500,
+  "Backend developer",
+  500,
+  "Problem solver",
+  500,
+];
+
+export {
+  navLinks,
+  services,
+  technologies,
+  experiences,
+  testimonials,
+  projects,
+  socials,
+  heroTexts,
+};
